bf6ab9381baef81b10c59f24fb63b03ad7c170c5
Xiriuaex <pragyanchetia22@gmail.com>
2024-01-30 08:45:21 +0530

    idk shit

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,9 @@ import { scrapeAmazonProduct } from "@/lib/scrapper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export async function GET() {
     try {
@@ -38,7 +41,7 @@ export async function GET() {
                 }
 
                 const updatedProduct = await Product.findOneAndUpdate(
-                    {url: scrapedProduct.url},
+                    {url: product.url},
                     product,
                 );
 
