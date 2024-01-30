2a49497ef6749194e599ada8dc569627ed9702f2
Xiriuaex <pragyanchetia22@gmail.com>
2024-01-30 14:31:06 +0530

    0

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -4,8 +4,7 @@ import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { scrapeAmazonProduct } from "@/lib/scrapper/index";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
-
-export const maxDuration = 10
+ 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
