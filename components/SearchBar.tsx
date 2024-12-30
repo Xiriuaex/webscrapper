11c7beaff82b9fcb807c51bbf9f5adef2bc4e7ae
Xiriuaex <pragyanchetia22@gmail.com>
2024-12-30 13:29:46 +0530

    searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,7 @@
-"use client";
+'use client'
 
-import { scrapeAndStoreProduct } from "@/lib/actions"; 
+import { scrapeAndStoreProduct } from "@/lib/actions";
+import { Product } from "@/types";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
@@ -10,40 +11,42 @@ const isValidAmazonProductURL = (url: string) => {
     const parsedURL = new URL(url);
     const hostname = parsedURL.hostname;
 
-    if (
-      hostname.includes("amazon.com") ||
-      hostname.includes("amazon.in") ||
-      hostname.includes("amazon.")
+    if(
+      hostname.includes('amazon.com')||
+      hostname.includes('amazon.in')||
+      hostname.includes('amazon.')
     ) {
+
       return true;
     }
   } catch (error) {
     return false;
   }
-};
+}; 
+ 
 
 const SearchBar = () => {
-  const { isSignedIn, user } = useUser();
-  const [searchPrompt, setSearchPrompt] = useState("");
+  const {isSignedIn, user} = useUser();
+  const [searchPrompt, setSearchPrompt] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
-
+  
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    if (!isSignedIn) {
-      alert("User Not logged!");
-      return;
-    }
+ 
+    if(!isSignedIn) {
+      alert("User Not logged!")
+      return 
+    }  
 
     const isValidLink = isValidAmazonProductURL(searchPrompt);
 
-    if (!isValidLink) return alert("Please provide a valid link");
+    if(!isValidLink) return alert("Please provide a valid link");
 
     try {
       setIsLoading(true);
-      const product = await scrapeAndStoreProduct(searchPrompt);
+      const product: Product | undefined = await scrapeAndStoreProduct(searchPrompt); 
 
       if (product) {
         router.push(`/product/${product.productId}`);
@@ -53,31 +56,24 @@ const SearchBar = () => {
     } catch (error) {
       console.log(error);
     } finally {
-      setIsLoading(false);
+      setIsLoading(false); 
     }
-  };
+  }
 
   return (
-    <form
-      className="flex flex-wrap gap-0 justify-start mt-12"
-      onSubmit={handleSubmit}
-    >
-      <input
-        type="text"
-        value={searchPrompt}
-        onChange={(e) => setSearchPrompt(e.target.value)}
-        placeholder="Enter product link"
-        className="flex-1 max-w-[500px] mr-2 p-3 border border-gray-500 rounded-lg shadow-xs text-base text-gray-500 focus:outline-none"
-      />
-      <button
-        type="submit"
-        className="bg-gray-900 w-[130px] border ml-2 border-gray-900 rounded-lg shadow-xs px-5 py-3 text-white text-base font-semibold hover:opacity-90 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-40"
-        disabled={searchPrompt === ""}
-      >
-        {isLoading ? "Searching..." : "Search"}
-      </button>
+    <form className="flex flex-wrap gap-0 justify-start mt-12" onSubmit={handleSubmit}>
+        <input 
+            type="text" 
+            value={searchPrompt}
+            onChange={(e) => setSearchPrompt(e.target.value)}
+            placeholder="Enter product link"
+            className="flex-1 max-w-[500px] mr-2 p-3 border border-gray-500 rounded-lg shadow-xs text-base text-gray-500 focus:outline-none"
+        />
+        <button type="submit" className="bg-gray-900 w-[130px] border ml-2 border-gray-900 rounded-lg shadow-xs px-5 py-3 text-white text-base font-semibold hover:opacity-90 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-40" disabled={searchPrompt === ''}>
+          {isLoading ? 'Searching...' : 'Search'}
+        </button>
     </form>
-  );
-};
+  )
+}
 
-export default SearchBar;
+export default SearchBar
